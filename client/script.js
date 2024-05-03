7888927cca546b66d3cacd11473865312a4625a9
muquitarif <muquitarif2@gmail.com>
2024-05-03 20:24:14 +0500

    initial commit

---
+++ client/script.js
@@ -0,0 +1,64 @@
+document.getElementById('connectionStatus').innerHTML = "Not Connected"
+
+const socket = new WebSocket('ws://192.168.137.113:80'); // Create a WebSocket connection
+
+// Update connection status when connection is established
+socket.addEventListener('open', function (event) {
+    console.log('Connection Established');
+    document.getElementById('connectionStatus').innerHTML = "Connected";
+});
+
+// Update connection status when connection is closed
+socket.addEventListener('close', function (event) {
+    console.log('WebSocket connection closed');
+    document.getElementById('connectionStatus').innerHTML = "Disconnected";
+});
+
+
+
+
+// Update connection status in case of error
+socket.addEventListener('error', function (error) {
+    console.error('WebSocket error:', error);
+    document.getElementById('connectionStatus').innerHTML = "Error";
+});
+
+if (socket.OPEN) 
+{
+    for (let i = 1; i <= 6; i++) {
+        let button = document.getElementById('table' + i);
+        button.addEventListener("click", function(event) {
+            socket.send(i.toString());
+            console.log(i.toString());
+        });
+    }
+
+    function addButtonClickListener(buttonId, data) {
+        document.getElementById(buttonId).addEventListener("click", function(event) {
+            socket.send(data);
+            console.log(data);
+        });
+    }
+    
+    addButtonClickListener('forward', 'F');
+    addButtonClickListener('backward', 'B');
+    addButtonClickListener('left', 'L');
+    addButtonClickListener('right', 'R');
+    addButtonClickListener('stop', 'S');
+}
+
+
+let logsList = document.getElementById('logs');
+socket.addEventListener('message', function(event) {
+    // The 'event' object contains the received data
+    let receivedData = event.data;
+
+    // Create a new <li> element to represent the new message
+    let newLogItem = document.createElement('li');
+    
+    // Set the text content of the <li> element to the received message
+    newLogItem.textContent = receivedData;
+    
+    // Append the new <li> element to the <ul> element
+    logsList.appendChild(newLogItem);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Robot Control App
+
+- Client Folder has Front-end code
+
+### Running the Application Locally
+
+- Write **npm install** in terminal to download all the dependent packages from package.json file in both client
+- Open the index.html file access the Robot App
+
+
+
